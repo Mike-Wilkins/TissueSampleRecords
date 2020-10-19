b339c8b59125f5a5230a83a812119434f01ac3b0
Mike <michael-wilkins@sky.com>
2020-10-19 21:24:19 +0100

    Collections InMemory test setup complete

--- TestSampleRecords.Tests/UnitTest1.cs
+++ TestSampleRecords.Tests/UnitTest1.cs
@@ -1,19 +1,62 @@
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
+using TissueSampleRecords.Models;
+using TissueSampleRecords.Repositories;
+using TissueSampleRecords.Services;
 using Xunit;
+
+
 
 namespace TestSampleRecords.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        List<CollectionModel> CollectionInMemoryDb()
         {
-            var a = 14;
-            var b = 5;
-            var c = a+b;
+            List<CollectionModel> collectionList = new List<CollectionModel>()
+            {
+                new CollectionModel
+                {
+                    Id = 1,
+                    Collection_Id = 1,
+                    Disease_Term = "Lymphoblastoid cell lines",
+                    Title = "Fit and well"
+                }
+            };
+            return collectionList;
+        }
 
-            Assert.Equal(19, c);
 
+
+        [Fact]
+        public void SQLCollectionRepository_Should_Add_Collection()
+        {
+            ICollectionRepository sut = GetInMemoryCollectionRepository();
+            List<CollectionModel> collection = CollectionInMemoryDb();
+
+            CollectionModel savedCollection = sut.Add(collection[0]);
+
+            Assert.Single(sut.GetAllCollections());
+            Assert.Equal("Lymphoblastoid cell lines", savedCollection.Disease_Term);
+
+        }
+
+
+
+
+        private ICollectionRepository GetInMemoryCollectionRepository()
+        {
+            DbContextOptions<ApplicationDbContext> options;
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            builder.UseInMemoryDatabase(databaseName: "CollectionDatabase");
+
+            options = builder.Options;
+            ApplicationDbContext collectionDataContext = new ApplicationDbContext(options);
+            collectionDataContext.Database.EnsureDeleted();
+            collectionDataContext.Database.EnsureCreated();
+
+            return new SQLCollectionRepository(collectionDataContext);
         }
     }
 }
